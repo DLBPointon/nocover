9c3741027beadafd959e6eaa15939ee18d8cd694
DLBPointon <damonlbp@hotmail.co.uk>
2025-10-21 22:46:48 +0100

    Linting fix for ty, lines do have listener as atribute

--- src/nocover/cli.py
+++ src/nocover/cli.py
@@ -49,8 +49,8 @@ def _get_the_logger(log_path: str) -> None:
 
     queue_handler = logging.getHandlerByName("queue_handler")
     if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
+        queue_handler.listener.start() #ty: ignore
+        atexit.register(queue_handler.listener.stop) #ty: ignore
 
 
 def main():
